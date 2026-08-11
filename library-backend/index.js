19a54f7dece566532015d95deb82c41e502252f9
Ronja <ronja.sonninen@helsinki.fi>
2026-08-11 16:32:25 +0300

    Ex 5 done

--- library-backend/index.js
+++ library-backend/index.js
@@ -116,7 +116,7 @@ const typeDefs = /* GraphQL */ `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks(author: String): [Book!]!
+    allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
 `;
@@ -126,10 +126,19 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: (root, args) => {
-      if (!args.author) {
+      if (!args.author && !args.genre) {
         return books;
       }
-      return books.filter((book) => book.author === args.author);
+      if (!args.genre) {
+        return books.filter((book) => book.author === args.author);
+      }
+      if (!args.author) {
+        return books.filter((book) => book.genres.includes(args.genre));
+      }
+      return books.filter(
+        (book) =>
+          book.author === args.author && book.genres.includes(args.genre)
+      );
     },
     allAuthors: () => authors,
   },
