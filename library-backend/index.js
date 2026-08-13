39b38656d2eb0f55fd2d19f089a21bad367f8e3e
Ronja <ronja.sonninen@helsinki.fi>
2026-08-13 14:23:48 +0300

    Ex 7 done

--- library-backend/index.js
+++ library-backend/index.js
@@ -128,6 +128,7 @@ const typeDefs = /* GraphQL */ `
       author: String!
       genres: [String!]!
     ): Book
+    editAuthor(name: String!, setBornTo: Int!): Author
   }
 `;
 
@@ -165,6 +166,16 @@ const resolvers = {
         authors = authors.concat(author);
       }
       return book;
+    },
+    editAuthor: (root, args) => {
+      const author = authors.find((author) => author.name === args.name);
+      if (!author) {
+        return null;
+      }
+
+      const updatedAuthor = { ...author, born: args.setBornTo };
+      authors = authors.map((a) => (a.name === args.name ? updatedAuthor : a));
+      return updatedAuthor;
     },
   },
 };
